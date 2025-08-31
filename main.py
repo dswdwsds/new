0a1cf7e092be679aecb9f2a9de16ee67880a4f1f
dswdwsds <bwdhal92@gmail.com>
2025-08-31 12:33:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,19 @@ remote_folder = "test1/episodes"
 repo_name_log = "abdo12249/test"
 missing_anime_log_filename = "missing_anime_log.json"
 
-HEADERS = {"User-Agent": "Mozilla/5.0"}
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
+    "Accept-Language": "en-US,en;q=0.9,ar;q=0.8",
+    "Referer": "https://google.com/",
+    "DNT": "1",
+    "Upgrade-Insecure-Requests": "1",
+    "Sec-Fetch-Dest": "document",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-Site": "none",
+    "Sec-Fetch-User": "?1"
+}
+
 scraper = cloudscraper.create_scraper()
 
 # --------------------
